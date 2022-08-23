4da33386c990c29fb789a06ab33df34e7ede2e5d
emorcos0 <103867283+emorcos0@users.noreply.github.com>
2022-08-23 14:27:21 -0700

    Final Test

--- fall21/csci_110/topic_1/test.cpp
+++ fall21/csci_110/topic_1/test.cpp
@@ -5,6 +5,7 @@ int main(int argc, char const *argv[])
 {
     std::cout << "Goodbye cruel world" << std::endl;
     std::cout << "Github repo test." << std::endl;
+    std::cout << "Github repo test 2." << std::endl;
     
 
     return 0;
