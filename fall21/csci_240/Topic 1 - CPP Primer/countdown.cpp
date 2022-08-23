2715d371f2b2d9e77e238a9025d34aedcddca004
Edward Morcos <edwardmorcos2004@gmail.com>
2022-08-23 14:30:36 -0700

    New Commit

---
+++ fall21/csci_240/Topic 1 - CPP Primer/countdown.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+//using std::cout;
+
+int main(int argc, char const *argv[])
+{
+    //std::cout << argc << " " << argv[0] << std::endl;
+    for (int i = 0; i < argc; ++i)
+        std::cout << argv[i] << std::endl;
+    
+    
+    std::cout << sizeof(argc) << " " << sizeof argv << std::endl;
+    return 0;
+}
